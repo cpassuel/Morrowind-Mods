fa10b41946fe2b6b6a12a417b330848d6f928b3a
cpassuel <cpassuel@laposte.net>
2023-05-01 15:05:16 +0200

    Add minimal constructor and mod infos

--- Sample Mod/main.lua
+++ Sample Mod/main.lua
@@ -4,3 +4,27 @@
 	@version	
 	@changelog	1.0 Initial version
 ]]--
+
+
+-- Informations about the mod
+local modName = "Sample Mod"
+local modVersion = "V1.0"
+local modConfig = "SampleMod"	-- for MCM config file
+local modAuthor= ""
+
+--[[
+
+	MWSE Lua ref https://mwse.github.io/MWSE/
+
+]]--
+
+
+
+--[[
+	constructor
+]]
+
+local function initialize()
+	-- code
+end
+event.register("initialized", initialize)
