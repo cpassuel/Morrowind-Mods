f048dc6da6a7ff4eacd54d32c78ae4597a769da4
cpassuel <cpassuel@laposte.net>
2023-05-01 21:09:10 +0200

    Add MCM UI code

--- Sample Mod/main.lua
+++ Sample Mod/main.lua
@@ -2,21 +2,26 @@
 	Sample mod
 	@author		
 	@version	
-	@changelog	1.0 Initial version
+	@changelog	0.0 Initial version
 ]]--
 
 
 -- Informations about the mod
 local modName = "Sample Mod"
-local modVersion = "V1.0"
+local modVersion = "V0.0"
 local modConfig = "SampleMod"	-- for MCM config file
-local modAuthor= ""
+local modAuthor= "me"
 
 --[[
 
 	MWSE Lua ref https://mwse.github.io/MWSE/
 
 ]]--
+
+
+--[[
+	event handlers 
+]]
 
 
 
@@ -28,3 +33,34 @@ local function initialize()
 	-- code
 end
 event.register("initialized", initialize)
+
+
+--[[
+	mod config menu
+]]
+
+local function createtableVar(id)
+	return mwse.mcm.createTableVariable{
+		id = id,
+		table = config
+	}  
+end
+
+local function registerModConfig()
+    local template = mwse.mcm.createTemplate(modName)
+	template:saveOnClose(modConfig, config)
+	
+	local page = template:createPage()
+	
+	local catMain = page:createCategory(modName)
+	catMain:createYesNoButton {
+		label = "Enable " .. modName,
+		description = "Allows you to Enable or Disable this mod",
+		variable = createtableVar("modEnabled"),
+		defaultSetting = true,
+	}
+	
+	mwse.mcm.register(template)
+end
+
+event.register("modConfigReady", registerModConfig)
