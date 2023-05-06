0f496f3801c64772964b081dffb51859427cd483
cpassuel <cpassuel@laposte.net>
2023-05-06 11:18:35 +0200

    Translation files for Sample Mod added

---
+++ Sample Mod/MWSE/mods/Sample Mod/i18n/deu.lua
@@ -0,0 +1,11 @@
+-- German version - It's from google translate
+-- Very basic translation file
+-- You can use the original text as key or a more concise version like below
+-- Translation in not complete because I'm lazy, it's just to show how it works
+
+return {
+    ["HELP_ATTACKED"] = "Hilfe, ich werde angegriffen",
+    ["BEWARE_SCUM"] = "Vorsicht, Abschaum !",
+    ["BUTTON_4"] = "Taste #4 gedrückt",
+    ["BUTTON_5"] = "Taste #5 gedrückt",
+}
---
+++ Sample Mod/MWSE/mods/Sample Mod/i18n/eng.lua
@@ -0,0 +1,11 @@
+--- English version
+-- Very basic translation
+-- You can use the original text as key or a more concise version like below
+-- Translation in not complete because I'm lazy, it's just to show how it works
+
+return {
+    ["HELP_ATTACKED"] = "Help, I'm attacked",
+    ["BEWARE_SCUM"] = "Beware, Scum !",
+    ["BUTTON_4"] = "Button #4 pressed",
+    ["BUTTON_5"] = "Button #5 pressed",
+}
