be5bbf62b9d4ff09ac38e52d2dd0f6abde11721a
cpassuel <cpassuel@laposte.net>
2023-06-04 18:42:53 +0200

    Initial commit

---
+++ QuickSecurity/MWSE/mods/QuickSecurity/i18n/deu.lua
@@ -0,0 +1,12 @@
+-- German version - It's from google translate
+
+return {
+    ["MCM.modEnabled.label"] = "Enable ",
+    ["MCM.modEnabled.description"] = "Allows you to Enable or Disable this mod",
+    ["MCM.useWorstCondition.label"] = "Equip the used tool first",
+    ["MCM.useWorstCondition.description"] = "Equip first already used tool to prevent having too many used tools in inventory by using a full condidtion each time",
+    ["MCM.computeChance.label"] = "Display chance to unlock or disarm",
+    ["MCM.computeChance.description"] = "Compute the chance to unlock the door/container to display only usable lockpicks",
+    ["Menu.title_probe"] = "Select a probe",
+    ["Menu.title_lockpick"] = "Select a lockpick",
+}
---
+++ QuickSecurity/MWSE/mods/QuickSecurity/i18n/fra.lua
@@ -0,0 +1,12 @@
+--- French version
+
+return {
+    ["MCM.modEnabled.label"] = "Activer ",
+    ["MCM.modEnabled.description"] = "Permet d'Activer ou de Désactiver ce mod",
+    ["MCM.useWorstCondition.label"] = "Equip the used tool first",
+    ["MCM.useWorstCondition.description"] = "Equip first already used tool to prevent having too many used tools in inventory by using a full condidtion each time",
+    ["MCM.computeChance.label"] = "Display chance to unlock or disarm",
+    ["MCM.computeChance.description"] = "Compute the chance to unlock the door/container to display only usable lockpicks",
+    ["Menu.title_probe"] = "Select a probe",
+    ["Menu.title_lockpick"] = "Select a lockpick",
+}
